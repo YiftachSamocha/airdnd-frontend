1a325d520cd557334e82fb85de5a39d799bf3941
Yiftach Samocha <yiftachsamo@gmail.com>
2024-09-03 14:25:30 +0300

    Improve reservation page

--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -1,26 +1,38 @@
-import { loadOrders } from "../store/actions/order.action";
+import { loadOrders, updateOrder } from "../store/actions/order.action";
 import { format } from "date-fns";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { AppHeader } from "../cmps/AppHeader";
+import { Link } from "react-router-dom";
 
 export function Reservations() {
     const reservations = useSelector(state => state.orderModule.orders)
     const currUser = useSelector(state => state.userModule.currUser)
-    const hostId = currUser._id
+    const [filterBy, setFilterBy] = useState({ type: 'all' })
+    let hostId = currUser ? currUser._id : ''
+
 
     useEffect(() => {
         loadOrders({ host: hostId })
-    }, [])
+    }, [currUser, reservations])
 
     function formatDate(date) {
         return format(date, 'yyyy-MM-dd')
     }
 
+    function changeStatus(stay, newStatus) {
+        updateOrder({ ...stay, status: newStatus })
+    }
+
     return <section className="reservations">
         <AppHeader />
-        {reservations.length === 0 ? <div>No reservations yet</div> : <div>
+        {!currUser ? <div>Log in to watch your reservations</div> : <div>
             <h2>Reservations</h2>
+            <div>
+                <button>Aprroved</button>
+                <button>Declined</button>
+                <button>Pending</button>
+            </div>
             <table>
                 <thead>
                     <tr>
@@ -42,13 +54,17 @@ export function Reservations() {
                             <th>{formatDate(reservation.startDate)}</th>
                             <th>{formatDate(reservation.endDate)}</th>
                             <th>{formatDate(reservation.createdAt)}</th>
-                            <th>{reservation.stay.name}</th>
+                            <th><Link to={'/stay/' + reservation.stay._id} >{reservation.stay.name}</Link></th>
                             <th>{reservation.totalPrice}</th>
-                            <th></th>
+                            <th>
+                                {reservation.status === 'pending' && <><button onClick={() => changeStatus(reservation, 'approved')} >Approve</button>
+                                    <button onClick={() => changeStatus(reservation, 'declined')} >Decline</button></>}
+                            </th>
                         </tr>
                     })}
                 </tbody>
             </table>
+            {reservations.length === 0 && <div>No reservations yet</div>}
         </div>}
     </section>
 
